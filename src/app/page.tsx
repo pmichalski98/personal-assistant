a557f9285905ce55f6e99aa5b242de26c40d40ee
pmichalski98 <dev.michalski98@gmail.com>
2023-10-31 18:00:24 +0100

    tauri app init

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-14 justify-center flex">
+      <div>
+        <Button>Click</Button>
+        <p>halo</p>
+      </div>
+    </main>
+  );
+}
