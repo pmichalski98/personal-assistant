675338539116a444a800d00a8df76dd5fcdd3bb6
piotr <piotrek.priv.michalski@wp.pl>
2023-11-04 22:27:08 +0100

    merge remains

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,12 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
-<<<<<<< HEAD
 import { SendIcon } from "lucide-react";
 import { FormEvent, useState } from "react";
-=======
-import { Mic, SendIcon } from "lucide-react";
-import { useState } from "react";
-import { OpenAIWhisperAudio } from "langchain/document_loaders/fs/openai_whisper_audio";
->>>>>>> 2a8281d12e4225d7ee3b8c45aed7fc09db6b54c5
 
 export default function Home() {
   const [prompt, setPrompt] = useState("");
 
-<<<<<<< HEAD
   async function handleSubmit(e: FormEvent) {
     e.preventDefault();
     try {
@@ -33,54 +26,6 @@ export default function Home() {
       setPrompt("");
     } catch (e) {
       console.log(e);
-=======
-  // async function getTranscript() {
-  //   if (!blob) return;
-  //   const loader = new OpenAIWhisperAudio(blob);
-
-  //   const docs = await loader.load();
-
-  //   console.log(docs);
-  // }
-
-  async function whisper() {
-    // const filePath = "./data/testaudio.mp3";
-
-    const loader = new OpenAIWhisperAudio(blob!);
-
-    const docs = await loader.load();
-
-    return docs[0].pageContent;
-  }
-
-  async function startRecording() {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-    });
-    setStream(stream);
-    const mediaRecorder = new MediaRecorder(stream);
-    setRecorder(mediaRecorder);
-    mediaRecorder.start();
-
-    const chunks: Blob[] = [];
-    mediaRecorder.ondataavailable = (e) => {
-      if (e.data.size > 0) chunks.push(e.data);
-    };
-    mediaRecorder.onstop = () => {
-      const blob = new Blob(chunks, { type: "audio/webm" });
-      setBlob(blob);
-      const blobUrl = URL.createObjectURL(blob);
-      setBlobUrl(blobUrl);
-    };
-  }
-
-  function stopRecording() {
-    if (recorder) {
-      recorder.stop();
-      recorder.stream.getTracks().map((track) => track.stop());
-      const text = whisper();
-      console.log(text);
->>>>>>> 2a8281d12e4225d7ee3b8c45aed7fc09db6b54c5
     }
   }
 
@@ -92,7 +37,6 @@ export default function Home() {
           <h2>Chat</h2>
           <Separator className="flex-1/3" />
         </div>
-<<<<<<< HEAD
         <div className="h-2/3"></div>
         <form onSubmit={handleSubmit} className="flex items-center relative">
           <Input
@@ -105,33 +49,6 @@ export default function Home() {
             <SendIcon />
           </Button>
         </form>
-=======
-        <div className="h-1/3">
-          {blobUrl && (
-            <audio controls>
-              {" "}
-              <source src={blobUrl} type="audio/mpeg" />
-            </audio>
-          )}
-        </div>
-        <div className="flex items-center relative">
-          <Input placeholder="Ask me anything..." className="" />
-          <div className="absolute top-0 flex justify-center items-center space-x-3 right-0">
-            <button
-              className="text-slate-400 hover:text-slate-600"
-              onClick={startRecording}
-            >
-              <Mic size="20" />
-            </button>
-            <Button
-              // onClick={(e) => handleClick(e)}
-              size="icon"
-            >
-              <SendIcon />
-            </Button>
-          </div>
-        </div>
->>>>>>> 2a8281d12e4225d7ee3b8c45aed7fc09db6b54c5
       </div>
     </main>
   );
